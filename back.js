ba374adf07f532a6604cd0ed1af40048dcc712ea
Raunak1375 <141954413+Raunak1375@users.noreply.github.com>
2023-10-15 11:21:30 +0530

    Add files via upload

---
+++ back.js
@@ -0,0 +1,8 @@
+function displayInfo() {
+    var info = document.getElementById("cancer-info");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
